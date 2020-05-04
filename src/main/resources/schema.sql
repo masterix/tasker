ffa99e04201469d4ba904ea065731d5af2721bf5
masterix <masterix.zone@gmail.com>
2020-05-04 22:17:59 +0200

    Add schema for tags and update previous DDL queries

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,8 @@
 DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS attachments;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS tags_tasks;
+
 CREATE TABLE tasks
 (
     id          IDENTITY,
@@ -7,9 +11,24 @@ CREATE TABLE tasks
     created_at  TIMESTAMP
 );
 
-CREATE TABLE attachments(
+CREATE TABLE attachments
+(
     filename VARCHAR(100) UNIQUE,
-    task NUMERIC,
-    comment VARCHAR,
+    task     NUMERIC,
+    comment  VARCHAR,
+    FOREIGN KEY (task) REFERENCES tasks (id)
+);
+
+CREATE TABLE tags
+(
+    id   IDENTITY,
+    name VARCHAR(100)
+);
+
+CREATE TABLE tags_tasks
+(
+    tag NUMERIC NOT NULL,
+    task NUMERIC NOT NULL,
+    FOREIGN KEY (tag) REFERENCES tags (id),
     FOREIGN KEY (task) REFERENCES tasks (id)
 );
